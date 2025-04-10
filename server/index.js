f6309ff9c2e13e8e738ce748009cf82f0e8a7058
orwrandal <randypichardo1987@gmail.com>
2025-04-10 13:52:42 -0400

    Done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@
 
 const path = require('path');
 const express = require('express');
+require("dotenv").config();
 
 //////////////////////////
 // Constants
@@ -12,14 +13,26 @@ const express = require('express');
 const port = 8080;
 const pathToDistFolder = path.join(__dirname, '../frontend/dist');
 const app = express();
+const {API_KEY} = process.env;
 
 //////////////////////////
 // Middleware/Controllers
 //////////////////////////
+const serveGifs = async (req, res) => {
+    try {
+        const url = `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        res.status(503).send(error);
+    }
+}
 
 const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
+app.get("/api/gifs", serveGifs);
 
 //////////////////////////
 // Listener
